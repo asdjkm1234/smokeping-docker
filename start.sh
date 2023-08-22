d86c0a3eb30378e9c277c654487514ed4ad62d84
asdjkm1234 <53368123+asdjkm1234@users.noreply.github.com>
2023-08-23 04:23:36 +0800

    尝试修复密码文件权限

--- start.sh
+++ start.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # 启动 Apache
-apachectl -D FOREGROUND &
+apachectl -D FOREGROUND
 
 # 设置密码文件权限
-chmod 600 /usr/local/smokeping/etc/smokeping_secrets.dist &
+chmod 600 /usr/local/smokeping/etc/smokeping_secrets.dist
+sleep 1
 
 # 启动 SmokePing
-/usr/local/smokeping/bin/smokeping /usr/local/smokeping/etc/config &
+/usr/local/smokeping/bin/smokeping /usr/local/smokeping/etc/config
 
 # 设置文件文件夹权限
-chown www-data:www-data -R /usr/local/smokeping &
+chown www-data:www-data -R /usr/local/smokeping
+sleep 1
 
 # 启动 tail 命令以实时监视 Apache 日志，并将日志输出到容器日志
 tail -f /var/log/apache2/access.log
